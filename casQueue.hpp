5800969d6416ed6dbdd6af95fa37c529defb31ca
Arrow-qaq <57163797+Arrow-qaq@users.noreply.github.com>
2023-03-31 17:04:02 +0800

    Update casQueue.hpp

--- casQueue.hpp
+++ casQueue.hpp
@@ -3,62 +3,54 @@
 
 template <typename T>
 class CASQueue {
-public:
+private:
     struct Node {
-        T data;
-        std::shared_ptr<Node> next;
-
-        Node(const T& data_) : data(data_) {}
+        std::unique_ptr<T> data;
+        Node* next;
+        Node(T* data): data(data), next(nullptr) {}
     };
-
-    CASQueue() {
-        std::shared_ptr<Node> dummy_node(new Node(T()));
-        tail = head = dummy_node;
+    std::atomic<Node*> head;
+    std::atomic<Node*> tail;
+public:
+    CASQueue(): head(new Node(nullptr)), tail(head.load()) {}
+    ~CASQueue() {
+        while (Node* const old_head = head.load()) {
+            head.store(old_head->next);
+            delete old_head;
+        }
     }
+    CASQueue(const CASQueue&) = delete;
+    CASQueue& operator=(const CASQueue&) = delete;
 
-    void push(const T& value) {
-        std::shared_ptr<Node> new_node(new Node(value));
-        std::shared_ptr<Node> old_tail = tail.load();
+    void enqueue(T* data) {
+        std::unique_ptr<Node> new_node(new Node(data));
+        Node* old_tail = tail.load();
         while (true) {
-            std::shared_ptr<Node> old_next = old_tail->next;
-            if (old_tail == tail.load()) {
-                if (old_next == nullptr) {
-                    if (std::atomic_compare_exchange_strong(
-                            &(old_tail->next), nullptr, new_node)) {
-                        break;
-                    }
-                } else {
-                    tail.compare_exchange_strong(old_tail, old_next);
+            Node* const next = old_tail->next;
+            if (!next) {
+                if (tail.compare_exchange_strong(old_tail, new_node.get())) {
+                    old_tail->next = new_node.release();
+                    return;
                 }
+            } else {
+                tail.compare_exchange_strong(old_tail, next);
             }
             old_tail = tail.load();
         }
-        tail.compare_exchange_strong(old_tail, new_node);
     }
 
-    std::shared_ptr<T> pop() {
-        std::shared_ptr<Node> old_head = head.load();
+    std::unique_ptr<T> dequeue() {
+        Node* old_head = head.load();
         while (true) {
-            std::shared_ptr<Node> old_tail = tail.load();
-            std::shared_ptr<Node> old_next = old_head->next;
-            if (old_head == head.load()) {
-                if (old_head == old_tail) {
-                    if (old_next == nullptr) {
-                        return nullptr;
-                    }
-                    tail.compare_exchange_strong(old_tail, old_next);
-                } else {
-                    std::shared_ptr<T> result(
-                        std::make_shared<T>(old_next->data));
-                    if (head.compare_exchange_strong(old_head, old_next)) {
-                        return result;
-                    }
-                }
+            Node* const head_next = old_head->next;
+            if (!head_next) {
+                return nullptr;
+            }
+            if (head.compare_exchange_strong(old_head, head_next)) {
+                std::unique_ptr<Node> old_head_ptr(old_head);
+                T* const res = old_head_ptr->data.release();
+                return std::unique_ptr<T>(res);
             }
         }
     }
-
-private:
-    std::atomic<std::shared_ptr<Node>> head;
-    std::atomic<std::shared_ptr<Node>> tail;
 };
