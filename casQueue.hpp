5b7434dd0a1c0551e45faad7be0bdb0ba7a89815
Arrow-qaq <57163797+Arrow-qaq@users.noreply.github.com>
2023-03-31 17:48:49 +0800

    Update casQueue.hpp

--- casQueue.hpp
+++ casQueue.hpp
@@ -21,38 +21,40 @@ public:
         auto new_node = std::make_shared<Node>();
         new_node->data = std::make_shared<T>(new_value);
 
+        Node* last = nullptr;
         while (true) {
-            Node* last = tail.load();
-            Node* next = last->next.load();
+            last = tail.load();
+            Node* next = last->next.load(std::memory_order_acquire);
 
-            if (last == tail) {
+            if (last == tail.load(std::memory_order_relaxed)) {
                 if (next == nullptr) {
                     if (last->next.compare_exchange_strong(next, new_node.get())) {
-                        tail.compare_exchange_strong(last, new_node.get());
-                        return;
+                        break;
                     }
                 } else {
                     tail.compare_exchange_strong(last, next);
                 }
             }
         }
+        tail.compare_exchange_strong(last, new_node.get(), std::memory_order_release);
     }
 
     std::shared_ptr<T> dequeue() {
+        Node* first = nullptr;
         while (true) {
-            Node* first = head.load();
-            Node* last = tail.load();
-            Node* next = first->next.load();
+            first = head.load(std::memory_order_acquire);
+            Node* last = tail.load(std::memory_order_acquire);
+            Node* next = first->next.load(std::memory_order_acquire);
 
-            if (first == head) {
+            if (first == head.load(std::memory_order_relaxed)) {
                 if (first == last) {
                     if (next == nullptr) {
                         return std::shared_ptr<T>();
                     }
-                    tail.compare_exchange_strong(last, next);
+                    tail.compare_exchange_strong(last, next, std::memory_order_release);
                 } else {
                     std::shared_ptr<T> res = next->data;
-                    if (head.compare_exchange_strong(first, next)) {
+                    if (head.compare_exchange_strong(first, next, std::memory_order_release)) {
                         return res;
                     }
                 }
